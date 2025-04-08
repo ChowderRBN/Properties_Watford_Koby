41e910ae415cec6c394ce01af4aaf5a4349bc68a
Koby Watford <kobywatford8@icloud.com>
2025-04-08 14:53:33 -0500

    Add files via upload

---
+++ Properties_Watford_Koby/Properties_Watford_Koby/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Properties_Watford_Koby
+{
+    public class Car
+    {
+        // Automatic property for 'Make' with getter and setter.
+        // The compiler will automatically create a backing field.
+        public string Make { get; set; }
+
+        // Automatic property for 'Model' with getter and setter.
+        // Like 'Make', the compiler will handle the backing field automatically.
+        public string Model { get; set; }
+    }
+}
---
+++ Properties_Watford_Koby/Properties_Watford_Koby/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Properties_Watford_Koby
+{
+    class Program
+    {
+        // Main method - the entry point for the application.
+        static void Main(string[] args)
+        {
+            // Create the first Car object.
+            // The car object 'car1' has the 'Make' set to 'Ford' and 'Model' set to 'Mustang'.
+            Car car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Mustang";
+
+            // Create the second Car object.
+            // This car object 'car2' has the 'Make' set to 'Chevy' and 'Model' set to 'Camero'.
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            // Create the third Car object.
+            // 'car3' has no 'Make' or 'Model' assigned, meaning these properties will be empty strings by default.
+            Car car3 = new Car();
+
+            // Print the details of the first car object (car1).
+            // The result will show 'Make: Ford' and 'Model: Mustang' because these properties were set.
+            Console.WriteLine($"Make: {car1.Make}, Model: {car1.Model}");
+
+            // Print the details of the second car object (car2).
+            // The result will show 'Make: Chevy' and 'Model: Camero' because these properties were set.
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            // Print the details of the third car object (car3).
+            // Since 'Make' and 'Model' were not set for car3, they will be empty strings by default.
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
+        }
+    }
+}
